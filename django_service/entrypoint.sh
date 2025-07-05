fafcbe0cc32afb9b0f6a350b8ea4f22f367829c5
Алексей Кольцов <koltsov2022@yandex.ru>
2025-07-05 14:09:34 +0300

    delete entrypoint.sh

--- django_service/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-poetry run python manage.py makemigrations
-poetry run python manage.py migrate
-poetry run python manage.py collectstatic --noinput
-
-exec poetry run gunicorn django_service.wsgi --bind 0.0.0.0:8000
